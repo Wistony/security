7d2be8d93298fc20cbc196fbc9bd4ef410aa8fb0
Wistony <42906954+Wistony@users.noreply.github.com>
2021-12-01 18:45:33 +0200

    genetic algorithm first realization added

---
+++ Lab1/Lab1/Chromosome.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab1
+{
+    public class Chromosome
+    {
+        public string key;
+        public string decryptedText;
+        private Dictionary<string, double> bigramDistribution;
+        private Dictionary<string, double> trigramDistribution;
+        public double fitness;
+
+        public Chromosome(string key, string encryptedText, Dictionary<string, double> englishTextBigramDistribution,
+            Dictionary<string, double> englishTextTrigramDistribution)
+        {
+            this.key = key;
+            DecodeText(encryptedText);
+
+            bigramDistribution = new Dictionary<string, double>();
+            trigramDistribution = new Dictionary<string, double>();
+            /*foreach (var t in englishTextBigramDistribution.Keys)
+            {
+                bigramDistribution[t] = 0;
+            }*/
+            CalculateBigramDistribution(englishTextBigramDistribution.Keys.ToList());
+            CalculateTrigramDistribution(englishTextTrigramDistribution.Keys.ToList());
+            CalculateFitness(englishTextBigramDistribution, englishTextTrigramDistribution);
+        }
+
+        private void DecodeText(string encryptedText)
+        {
+            var charArray = new char[encryptedText.Length];
+            for (var i = 0; i < key.Length; i++)
+            {
+                for (var j = 0; j < encryptedText.Length; j++)
+                {
+                    if (key[i] == encryptedText[j])
+                    {
+                        charArray[j] = (char) ('A' + i);
+                    }
+                }
+            }
+
+            decryptedText = new string(charArray);
+        }
+
+        private void CalculateBigramDistribution(List<string> allBigram)
+        {
+            for (var i = 0; i < decryptedText.Length - 1; i++)
+            {
+                var bigram = decryptedText.Substring(i, 2);
+                if (!bigramDistribution.ContainsKey(bigram))
+                {
+                    bigramDistribution[bigram] = 1;
+                }
+                else
+                {
+                    bigramDistribution[bigram] += 1;
+                }
+            }
+            var bigramCount = decryptedText.Length - 1;
+
+            foreach (var bigram in allBigram)
+            {
+                if (bigramDistribution.ContainsKey(bigram))
+                {
+                    bigramDistribution[bigram] = bigramDistribution[bigram] / bigramCount;
+                }
+                else
+                {
+                    bigramDistribution[bigram] = 0;
+                }
+            }
+        }
+        
+        private void CalculateTrigramDistribution(List<string> allTrigram)
+        {
+            for (var i = 0; i < decryptedText.Length - 2; i++)
+            {
+                var trigram = decryptedText.Substring(i, 3);
+                if (!trigramDistribution.ContainsKey(trigram))
+                {
+                    trigramDistribution[trigram] = 1;
+                }
+                else
+                {
+                    trigramDistribution[trigram] += 1;
+                }
+            }
+            var trigramCount = decryptedText.Length - 2;
+            foreach (var trigram in allTrigram)
+            {
+                if (trigramDistribution.ContainsKey(trigram))
+                {
+                    trigramDistribution[trigram] = trigramDistribution[trigram] / trigramCount;
+                }
+                else
+                {
+                    trigramDistribution[trigram] = 0;
+                }
+            }
+        }
+
+        private void CalculateFitness(Dictionary<string, double> englishTextBigramDistribution,Dictionary<string, double> englishTextTrigramDistribution)
+        {
+            fitness = 0;
+            foreach (var k in englishTextBigramDistribution.Keys)
+            {
+                fitness += Math.Abs(englishTextBigramDistribution[k] * 100 - bigramDistribution[k] * 100);
+            }
+            foreach (var k in englishTextTrigramDistribution.Keys)
+            {
+                fitness += Math.Abs(englishTextTrigramDistribution[k] * 100 - trigramDistribution[k] * 100);
+            }
+        }
+    }
+}
---
+++ Lab1/Lab1/GeneticAlgorithm.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    public class GeneticAlgorithm
+    {
+        private const int PopulationSize = 20;
+        private double mutationRate = 0.5; //percent
+        private int numOfSwaps = 2;
+        private string Alphabet;
+        private string encryptedText;
+        private List<Chromosome> population;
+
+        private Dictionary<string, double> bigramDistribution;
+        private Dictionary<string, double> trigramDistribution;
+
+        private Chromosome bestKey;
+        private int withoutNewBest;
+
+        public GeneticAlgorithm(string encryptedText)
+        {
+            this.encryptedText = encryptedText;
+            population = new List<Chromosome>();
+            Alphabet = "";
+            withoutNewBest = 0;
+
+            CalculateBigramDistribution();
+            CalculateTrigramDistribution();
+
+            for (var i = 0; i < 26; i++)
+            {
+                Alphabet += (char) ('A' + i);
+            }
+            for (var i = 0; i < PopulationSize; i++)
+            {
+                var randomKey = GenerateRandomKeys();
+                population.Add(new Chromosome(randomKey, encryptedText,bigramDistribution,trigramDistribution));
+            }
+
+            bestKey = population[0];
+            run();
+        }
+
+        public void run()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                Crossover();
+                Console.WriteLine("=========== " + i + "==============");
+                Console.WriteLine("Without New Best: " + withoutNewBest);
+                Console.WriteLine("BestFitness: " + bestKey.fitness);
+                Console.WriteLine("Key: " + bestKey.key);
+                Console.WriteLine("Text: " + bestKey.decryptedText);
+                if (withoutNewBest > 50 && numOfSwaps < 10)
+                {
+                    withoutNewBest = 0;
+                    numOfSwaps += 1;
+                }
+
+                if (withoutNewBest > 100)
+                {
+                    withoutNewBest = 0;
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("AAAAAAAAAAAAAAAAAAAAA SGDG ______");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    population.Sort((a, b) => a.fitness >= b.fitness ? 1 : -1);
+                    var list = new List<Chromosome>();
+                    for (var j = 0; j < PopulationSize; j++)
+                    {
+                        if (j < 0.2 * PopulationSize)
+                        {
+                            list.Add(population[j]);
+                        }
+                        else
+                        {
+                            var randomKey = GenerateRandomKeys();
+                            list.Add(new Chromosome(randomKey, encryptedText,bigramDistribution,trigramDistribution));
+                        }
+                    }
+
+                    population = list;
+                }
+            }
+        }
+
+        private void CalculateBigramDistribution()
+        {
+            bigramDistribution = new Dictionary<string, double>();
+            var sr =
+                new StreamReader(
+                    "C:\\Users\\MaksymAndroshchuk\\OneDrive - Ladoburn Europe Ltd\\Desktop\\security\\Lab1\\Lab1\\Files\\english_bigrams.txt");
+
+            var line = sr.ReadLine();
+            
+            while (line != null)
+            {
+                var str = line.Split(" ");
+                bigramDistribution[str[0]] = Convert.ToDouble(str[1]);
+                line = sr.ReadLine();
+            }
+
+            var sum = bigramDistribution.Sum(x => x.Value);
+
+            foreach (var key in bigramDistribution.Keys.ToList())
+            {
+                bigramDistribution[key] = bigramDistribution[key] / sum;
+            }
+        }
+        
+        private void CalculateTrigramDistribution()
+        {
+            trigramDistribution = new Dictionary<string, double>();
+            var sr =
+                new StreamReader(
+                    "C:\\Users\\MaksymAndroshchuk\\OneDrive - Ladoburn Europe Ltd\\Desktop\\security\\Lab1\\Lab1\\Files\\english_trigrams.txt");
+            
+            var line = sr.ReadLine();
+            
+            while (line != null)
+            {
+                var str = line.Split(" ");
+                trigramDistribution[str[0]] = Convert.ToDouble(str[1]);
+                line = sr.ReadLine();
+            }
+
+            var sum = trigramDistribution.Sum(x => x.Value);
+
+            foreach (var key in trigramDistribution.Keys.ToList())
+            {
+                trigramDistribution[key] = trigramDistribution[key] / sum;
+            }
+        }
+
+        public Chromosome CrossTwoParent(Chromosome parent1, Chromosome parent2)
+        {
+            var r = new Random();
+            var rInt = r.Next(1, 26);
+            
+            var newKey = parent1.key[..rInt];
+            foreach (var ch in parent2.key)
+            {
+                if (!newKey.Contains(ch))
+                    newKey += ch;
+            }
+            
+            return new Chromosome(newKey, encryptedText, bigramDistribution,trigramDistribution);
+        }
+        
+        public void Crossover()
+        {
+            var children = new List<Chromosome>();
+            for (var i = 0; i < PopulationSize; i++)
+            {
+                var (parent1, parent2) = TournamentSelection();
+                
+                var child1 = CrossTwoParent(parent1, parent2);
+                while(children.Any(ch => ch.key == child1.key))
+                {
+                    (parent1, parent2) = TournamentSelection();
+                    child1 = CrossTwoParent(parent1, parent2);
+                }
+                children.Add(child1);
+                
+                var child2 = CrossTwoParent(parent2, parent1);
+
+                while(children.Any(ch => ch.key == child2.key))
+                {
+                    (parent1, parent2) = TournamentSelection();
+                    child2 = CrossTwoParent(parent2, parent1);
+                }
+                
+                children.Add(child2);
+            }
+
+            var childrenAndParent = children.Concat(population).ToList();
+            childrenAndParent.Sort((a, b) => a.fitness >= b.fitness ? 1 : -1);
+
+            var newGeneration = new List<Chromosome>();
+            for (var i = 0; i < PopulationSize; i++)
+            {
+                newGeneration.Add(childrenAndParent[i]);
+
+            }
+
+            if (bestKey.fitness > newGeneration[0].fitness)
+            {
+                bestKey = newGeneration[0];
+                withoutNewBest = 0;
+            }
+            else
+            {
+                withoutNewBest += 1;
+            }
+            
+            var rnd = new Random();
+            population = newGeneration.OrderBy(item => rnd.Next()).ToList();
+            for (var i = 0; i < PopulationSize * mutationRate; i++)
+            {
+                mutation();
+            }
+        }
+
+        public void mutation()
+        {
+            var r = new Random();
+            var rChrom = r.Next(0, PopulationSize);
+
+            for (var i = 0; i < numOfSwaps; i++)
+            {
+                swapGenes(rChrom);
+            }
+            
+        }
+
+        public void swapGenes(int rChrom)
+        {
+            var r = new Random();
+
+            var rGen = r.Next(0, 26);
+            var rGen1 = r.Next(0, 26);
+            while (rGen == rGen1)
+            {
+                rGen1 = r.Next(0, 26);
+            }
+
+            var ch = population[rChrom].key;
+
+            StringBuilder str = new StringBuilder(ch);
+            var temp = str[rGen];
+            str[rGen] = str[rGen1];
+            str[rGen1] = temp;
+
+            population[rChrom] = new Chromosome(str.ToString(), encryptedText, bigramDistribution,trigramDistribution);
+        }
+
+        public (Chromosome, Chromosome) TournamentSelection()
+        {
+            var r = new Random();
+            var rInt1 = r.Next(0, PopulationSize);
+            var rInt2 = r.Next(0, PopulationSize);
+            var parent1 = population[rInt1].fitness >= population[rInt2].fitness ?
+                population[rInt2] : population[rInt1];
+            
+            rInt1 = r.Next(0, PopulationSize);
+            rInt2 = r.Next(0, PopulationSize);
+            
+            var parent2 = population[rInt1].fitness >= population[rInt2].fitness ?
+                population[rInt2] : population[rInt1];
+
+            var count = 0;
+            while (parent1.key == parent2.key && count < 3)
+            {
+                rInt1 = r.Next(0, PopulationSize);
+                rInt2 = r.Next(0, PopulationSize);
+                parent1 = population[rInt1].fitness >= population[rInt2].fitness ? population[rInt2] : population[rInt1];
+                count++;
+            }
+
+            return (parent1, parent2);
+        }
+        private string GenerateRandomKeys()
+        {
+            var q = from c in Alphabet.ToCharArray()
+                orderby Guid.NewGuid()
+                select c;
+            var s = string.Empty;
+            foreach (var r in q)
+                s += r;
+
+            return s;
+        }
+    }
+}
